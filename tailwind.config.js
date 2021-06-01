83faaa066fae4bfd4617b5649f78fefbd5fa4cb7
Joshua Wiegand <josh.r.wiegand@gmail.com>
2021-05-31 22:32:06 -0400

    fix tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   purge: {
     enabled: true,
     content: ["./templates/base.html", "./templates/index.html"],
-    theme: {
-      container: {
-        center: true,
-        padding: "2rem",
-      },
+  },
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
     },
   },
 };
